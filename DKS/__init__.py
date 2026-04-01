9262138f9670d5a79344723146a0a48d34ebfe03
dakeypunchar <0dakeypunchar0@gmail.com>
2026-04-02 00:08:40 +0530

    Authentication + Basic Index page

--- DKS/__init__.py
+++ DKS/__init__.py
@@ -1,6 +1,9 @@
 import os
 from flask import Flask
-
+from flask import render_template
+from flask import session
+from flask import redirect
+from flask import url_for
 
 def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
@@ -16,8 +19,35 @@ def create_app(test_config=None):
 
     os.makedirs(app.instance_path, exist_ok=True)
 
+    # register the database commands
+    from . import db
+    db.init_app(app)
+
+    # apply the blueprints to the app
+    from . import auth
+    app.register_blueprint(auth.bp)
+
     @app.route('/')
-    def hello():
-        return 'Hello, World!'
+    def index():
+        if 'user_id' not in session:
+            return redirect(url_for('auth.login'))
+
+        user_id = session['user_id']
+
+        from .db import get_db
+        db = get_db()
+        user = db.execute('SELECT * FROM user WHERE id = ?',
+                          (user_id,)).fetchone()
+
+        if user is None:
+            session.clear()
+            return redirect(url_for('auth.login'))
+
+        name = user['username']
+        return render_template('index.html', username = name, tasks = [])
+
+    @app.route('/add_task')
+    def add_task():
+        return render_template('add_task.html')
 
     return app
